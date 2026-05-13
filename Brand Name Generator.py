a011e14cc74cd79bc4359b7080d45c1b24258427
SARAVANA D <saravanakd444@gmail.com>
2026-05-13 14:09:15 +0530

    Update Brand Name Generator.py

--- Brand Name Generator.py
+++ Brand Name Generator.py
@@ -7,6 +7,7 @@ actor = input("Who's your favorite actor? ")
 actress = input("Who's your favorite actress? ")
 state = input("Which state are you from? ")
 
+
 print(f"""
 Once upon a time, there lived a man called {actor}.
 He was immortal and god-gifted.
